565e2e93523066b9b147c0f365d8c2feda596937
Guilherme Guimarães <guilherme.mendes@seduc.ce.gov.br>
2026-04-30 16:38:57 -0300

    Deploy da aplicação Angular

--- src/app/categorias/categoria.service.ts
+++ src/app/categorias/categoria.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Categoria } from './categoria';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -9,14 +10,13 @@ import { Observable } from 'rxjs';
 export class CategoriaService {
   constructor(private http: HttpClient) {}
 
+  apiUrl: string = environment.apiUrl + '/categorias';
+
   salvar(categoria: Categoria): Observable<Categoria> {
-    return this.http.post<Categoria>(
-      'http://localhost:3000/categorias',
-      categoria,
-    );
+    return this.http.post<Categoria>(this.apiUrl, categoria);
   }
 
   listar(): Observable<Categoria[]> {
-    return this.http.get<Categoria[]>('http://localhost:3000/categorias');
+    return this.http.get<Categoria[]>(this.apiUrl);
   }
 }
